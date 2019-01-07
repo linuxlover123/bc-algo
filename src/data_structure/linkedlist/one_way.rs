05ad3cc503b0b8f85566a6b5bcd18fbb20774f47
fanhui <hui.fan@mail.ru>
2019-01-07 18:28:44 +0800

    opti v0.1 single thread version

--- src/data_structure/linkedlist/one_way.rs
+++ src/data_structure/linkedlist/one_way.rs
@@ -1,39 +1,86 @@
-enum List {
-    Cons(u64, Box<List>),
-    NULL,
+use std::{
+    rc::Rc,
+    cell::RefCell
+};
+
+type SizeT=u64;
+type DataT=i32;
+
+struct List {
+    len: SizeT ,
+    header: Rc<RefCell<Node>>,
+    tail: Rc<RefCell<Node>>,
+}
+
+#[derive(Clone)]
+enum Node {
+    Nil,
+    Obj(DataT, Rc<RefCell<Node>>),
 }
 
 impl List {
-    fn new() -> List {
-        List::NULL
-    }
-
-    fn prepend(self, elem: u64) -> List {
-        List::Cons(elem, Box::new(self))
-    }
-
-    fn len(&self) -> u64 {
-        let mut t = self;
-        let mut res = 0u64;
-
-        while let List::Cons(_, ref next) = *t {
-            res += 1;
-            t = next;
+    pub fn new() -> List {
+        List {
+            len: 0,
+            header: Rc::new(RefCell::new(Node::Nil)),
+            tail: Rc::new(RefCell::new(Node::Nil)),
         }
-
-        res
     }
 
-    fn stringify(&self) -> String {
-        let mut t = self;
+    // 正向追加节点
+    pub fn prevadd(&mut self, data: DataT) {
+        self.len += 1;
+        self.header = Rc::new(RefCell::new(Node::Obj(data, Rc::clone(&self.header))));
+
+        // optimize to execute once lazily?
+        if 1 == self.len {
+            self.tail = Rc::clone(&self.header);
+        }
+    }
+
+    // 反向追加节点
+    pub fn backadd(&mut self, data: DataT) {
+        if 0 == self.len {
+            self.prevadd(data);
+        } else {
+            self.len += 1;
+
+            let keep = Rc::clone(&self.tail);
+            if let Node::Obj(_, ref p) = *keep.borrow() {
+                *p.borrow_mut() = Node::Obj(data, Rc::new(RefCell::new(Node::Nil)));
+                self.tail = Rc::clone(p);
+            } else {
+                panic!("BUG!");
+            }
+
+            let _ = keep;
+        }
+    }
+
+    // 弹出最前面的节点
+    pub fn prevpop(&mut self) -> DataT {
+        unimplemented!();
+    }
+
+    // 弹出最后面的节点
+    pub fn backpop(&mut self) -> DataT {
+        unimplemented!();
+    }
+
+    pub fn len(&self) -> SizeT {
+        self.len
+    }
+
+    pub fn stringify(&self) -> String {
         let mut res = String::new();
 
-        while let List::Cons(r, ref next) = *t {
-            res.push_str(&format!("{}==>", r));
-            t = next;
+        let mut p = Rc::clone(&self.header).borrow().clone();
+        while let Node::Obj(node, next) = p {
+            res.push_str(&format!("{}==>", node));
+            p = Rc::clone(&next).borrow().clone();
         }
 
-        res.push_str(&format!("NULL"));
+        res.push_str(&format!("Nil"));
         res
     }
 }
@@ -45,11 +92,23 @@ mod tests {
     #[test]
     fn test() {
         let mut list = List::new();
-        for x in 0..10000 {
-            list = list.prepend(x);
+        for x in 0..=99 {
+            list.prevadd(x);
         }
+        assert_eq!(list.len, 100);
+
+        for x in 1..=100 {
+            list.backadd(-x);
+        }
+        assert_eq!(list.len, 200);
+
+        //assert_eq!(99, list.prevpop());
+        //assert_eq!(98, list.prevpop());
+        //assert_eq!(97, list.prevpop());
+        //assert_eq!(-100, list.backpop());
+        //assert_eq!(-99, list.backpop());
+        //assert_eq!(-99, list.backpop());
 
         println!("{}", list.stringify());
-        println!("linked list has length: {}", list.len());
     }
 }
