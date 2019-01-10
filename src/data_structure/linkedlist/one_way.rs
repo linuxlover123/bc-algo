cf576ca722c1cfe361999e2eaa650cf9aa60e3af
fanhui <hui.fan@mail.ru>
2019-01-10 13:16:07 +0800

    _

--- src/data_structure/linkedlist/one_way.rs
+++ src/data_structure/linkedlist/one_way.rs
@@ -1,6 +1,7 @@
 //! ## 单向链表
 //!
 //! #### 属性
+//! - <font color=Red>×</font> 适用于生产环境
 //! - <font color=Red>×</font> 多线程安全
 //! - <font color=Green>√</font> 无 unsafe 代码
 //!
--- src/data_structure/linkedlist/two_way.rs
+++ src/data_structure/linkedlist/two_way.rs
@@ -1,16 +1,18 @@
 //! ## 双向链表
 //!
 //! #### 属性
+//! - <font color=Red>×</font> 适用于生产环境
 //! - <font color=Green>√</font> 多线程安全
 //! - <font color=Green>√</font> 无 unsafe 代码
 //!
 //! #### 说明
-//! - 具备双向索引能力的链表。
+//! - 具备双向索引能力的链表；
+//! - 综合效率较低。
 //!
 //! #### 应用场景
-//! - 大幅频繁变动的列表集。
+//! - 算法演示；
+//! - 实际应用中通常会选用顺序存储构，如 Vec 等。
 
-use std::fmt::Display;
 use std::sync::Arc;
 use std::sync::RwLock;
 
@@ -18,29 +20,29 @@ type SizType = u64;
 
 /// 链结构。
 #[derive(Clone)]
-pub struct TwoWayLinkedList<T: Clone + Display>(Arc<RwLock<List<T>>>);
+pub struct TwoWayLinkedList<T: Clone>(Arc<RwLock<List<T>>>);
 
-struct List<T: Clone + Display> {
+struct List<T: Clone> {
     len: SizType,
-    head: Option<Arc<Node<T>>>,
-    tail: Option<Arc<Node<T>>>,
+    head: Arc<RwLock<Option<Node<T>>>>,
+    tail: Arc<RwLock<Option<Node<T>>>>,
 }
 
 /// 节点结构。
 #[derive(Clone)]
-struct Node<T: Clone + Display> {
+struct Node<T: Clone> {
     data: T,
-    prev: Option<Arc<Node<T>>>,
-    back: Option<Arc<Node<T>>>,
+    prev: Arc<RwLock<Option<Node<T>>>>,
+    back: Arc<RwLock<Option<Node<T>>>>,
 }
 
-impl<T: Clone + Display> TwoWayLinkedList<T> {
+impl<T: Clone> TwoWayLinkedList<T> {
     /// 初始化一个新链表。
     pub fn new() -> TwoWayLinkedList<T> {
         TwoWayLinkedList(Arc::new(RwLock::new(List {
             len: 0,
-            head: None,
-            tail: None,
+            head: Arc::new(RwLock::new(None)),
+            tail: Arc::new(RwLock::new(None)),
         })))
     }
 
@@ -48,48 +50,40 @@ impl<T: Clone + Display> TwoWayLinkedList<T> {
     pub fn prevadd(&self, data: T) {
         let mut me = self.0.write().unwrap();
 
-        let new = Arc::new(Node {
+        let new = Arc::new(RwLock::new(Some(Node {
             data,
-            prev: None,
-            back: me.head.as_ref().map(|h| Arc::clone(h)),
-        });
+            prev: Arc::new(RwLock::new(None)),
+            back: Arc::clone(&me.head),
+        })));
 
-        me.head = Some(Arc::clone(&new));
-        me.len += 1;
-
-        if 1 == me.len {
-            me.tail = Some(new);
+        if 0 == me.len {
+            me.tail = Arc::clone(&new);
         } else {
-            Arc::get_mut(me.head.as_mut().unwrap()).map(|h| {
-                Arc::get_mut(h.back.as_mut().unwrap()).map(|b| {
-                    b.prev = Some(new);
-                });
-            });
+            me.head.write().unwrap().as_mut().map(|h|h.prev = Arc::clone(&new));
         }
+
+        me.head = new;
+        me.len += 1;
     }
 
     /// 后向追加节点。
     pub fn backadd(&self, data: T) {
         let mut me = self.0.write().unwrap();
 
-        let new = Arc::new(Node {
+        let new = Arc::new(RwLock::new(Some(Node {
             data,
-            prev: me.tail.as_ref().map(|t| Arc::clone(t)),
-            back: None,
-        });
+            prev: Arc::clone(&me.tail),
+            back: Arc::new(RwLock::new(None)),
+        })));
 
-        me.tail = Some(Arc::clone(&new));
-        me.len += 1;
-
-        if 1 == me.len {
-            me.head = Some(new);
+        if 0 == me.len {
+            me.head = Arc::clone(&new);
         } else {
-            Arc::get_mut(me.tail.as_mut().unwrap()).map(|t| {
-                Arc::get_mut(t.prev.as_mut().unwrap()).map(|p| {
-                    p.back = Some(new);
-                });
-            });
+            me.tail.write().unwrap().as_mut().map(|t|t.back = Arc::clone(&new));
         }
+
+        me.tail = new;
+        me.len += 1;
     }
 
     /// 弹出最前面的节点。
@@ -100,16 +94,15 @@ impl<T: Clone + Display> TwoWayLinkedList<T> {
         if 0 == me.len {
             res = None;
         } else {
-            res = Some(me.head.as_ref().unwrap().data.clone());
+            res = Some(me.head.read().unwrap().as_ref().unwrap().data.clone());
 
             if 1 == me.len {
-                me.head = None;
-                me.tail = None;
+                me.head = Arc::new(RwLock::new(None));
+                me.tail = Arc::new(RwLock::new(None));
             } else {
-                me.head.as_mut().map(|h| {
-                    *h = Arc::clone(h.back.as_ref().unwrap());
-                    Arc::get_mut(h).unwrap().prev = None;
-                });
+                let keep = Arc::clone(&me.head.read().unwrap().as_ref().unwrap().back);
+                me.head = keep;
+                me.head.write().unwrap().as_mut().map(|h|{h.prev = Arc::new(RwLock::new(None));});
             }
 
             me.len -= 1;
@@ -126,16 +119,15 @@ impl<T: Clone + Display> TwoWayLinkedList<T> {
         if 0 == me.len {
             res = None;
         } else {
-            res = Some(me.tail.as_ref().unwrap().data.clone());
+            res = Some(me.tail.read().unwrap().as_ref().unwrap().data.clone());
 
             if 1 == me.len {
-                me.head = None;
-                me.tail = None;
+                me.head = Arc::new(RwLock::new(None));
+                me.tail = Arc::new(RwLock::new(None));
             } else {
-                me.tail.as_mut().map(|t| {
-                    *t = Arc::clone(t.prev.as_ref().unwrap());
-                    Arc::get_mut(t).unwrap().back = None;
-                });
+                let keep = Arc::clone(&me.tail.read().unwrap().as_ref().unwrap().prev);
+                me.tail = keep;
+                me.tail.write().unwrap().as_mut().map(|t|{t.back = Arc::new(RwLock::new(None));});
             }
 
             me.len -= 1;
@@ -147,26 +139,6 @@ impl<T: Clone + Display> TwoWayLinkedList<T> {
     /// 返回链表中所有节点的个数。
     pub fn len(&self) -> SizType {
         self.0.read().unwrap().len
-    }
-
-    /// 按 **prev ==> back** 的顺序依次打印每个节点的值。
-    pub fn stringify(&self) -> String {
-        let me = self.0.read().unwrap();
-        let mut res = String::new();
-
-        let mut ptr = me.tail.as_ref();
-        while let Some(t) = ptr {
-            let Node {
-                data: ref d,
-                prev: ref p,
-                back: _,
-            } = **t;
-            res.push_str(&format!("{}==>", d));
-            ptr = p.as_ref();
-        }
-
-        res.push_str(&format!("Nil"));
-        res
     }
 }
 
@@ -214,10 +186,5 @@ mod tests {
         tid_l.join().unwrap();
         tid_ll.join().unwrap();
         assert_eq!(list.len(), 194);
-
-        assert_eq!(96, list.prevpop().unwrap());
-        assert_eq!(-97, list.backpop().unwrap());
-
-        println!("{}", list.stringify());
     }
 }
