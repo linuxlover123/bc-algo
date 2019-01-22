bea4eb1cb6b2addb37b32f0088f31d2c4ff157f2
linuxforever <343708149@qq.com>
2019-01-22 18:51:33 +0800

    draft

--- src/data_structure/tree/huffman.rs
+++ src/data_structure/tree/huffman.rs
@@ -11,20 +11,18 @@
 //! - <font color=Green>√</font> 无 unsafe 代码
 
 type Byte = u8;
-type ByteLen = u8; //[0, 7]
-const ByteSiz: usize = std::mem::size_of::<BYTE>();
+const ByteSiz: usize = std::mem::size_of::<Byte>();
 
 //> 预置bit集合，优化位运算的效率
 const BitSet: [Byte; 8] = [
-    b00000001, b00000010, b00000100, b00001000, b00010000, b00100000, b01000000, b10000000,
+    0b00000001, 0b00000010, 0b00000100, 0b00001000, 0b00010000, 0b00100000, 0b01000000, 0b10000000,
 ];
 
 //> 以Byte为对象进行编解码的抽象数据类型，适配所有的数据类型
 type HuffmanTree = Node;
-struct Node {
+pub struct Node {
     left: Option<Box<Node>>,
     right: Option<Box<Node>>,
-
     data: Option<Byte>,
 }
 
@@ -33,37 +31,142 @@ type EncodeTable = [Vec<Byte>; 256];
 //> 解码表的用途仅是还原huffman tree，无需索引，亦与存储顺序无关
 type DecodeTable = Vec<(Vec<Byte>, Byte)>;
 
+//> 解码需要的信息
+pub struct Encoded {
+    //encoded data received from some sender[s]
+    data: Vec<Byte>,
+    //the number of bit[s] at the last byte for aligning to ByteSiz
+    pad_len: usize,
+}
+
+macro_rules! walk_on_tree {
+    ($encoded: expr, $t: expr, $byte_idx: expr, $bit_idx: expr) => {
+        if check_bit($encoded.data[$byte_idx], $bit_idx) {
+            if let Some(ref node) = $t.right {
+                $t = node;
+            } else {
+                return Err(());
+            }
+        } else {
+            if let Some(ref node) = $t.left {
+                $t = node;
+            } else {
+                return Err(());
+            }
+        }
+
+        $bit_idx += 1;
+        $bit_idx %= ByteSiz;
+        if 0 == $bit_idx {
+            $byte_idx += 1;
+        }
+        if $byte_idx >= $encoded.data.len() {
+            break;
+        }
+    };
+}
+
 pub trait Huffman {
     //> generate en[de]code-table
     //- @data：用于生成(编/解)码表的样本数据集
-    fn gen_table(data: &[Byte]) -> (EncodeTable, DecodeTable) {}
+    fn gen_table(data: &[Byte]) -> (EncodeTable, DecodeTable) {
+        unimplemented!();
+    }
 
     //> restore the HuffmanTree from a decode-table
-    //- @dt: 由同一样本数据生成的或从编码方获取的码表
-    fn restore_tree(dt: DecodeTable) -> HuffmanTree {}
+    //- @table[in]: code-table for decompression
+    fn restore_tree(table: DecodeTable) -> HuffmanTree {
+        unimplemented!();
+    }
 
-    //
-    //
-    fn encode(data: &[Byte]) -> Vec<Byte> {}
+    //> 基本的编码函数——单线程、数据不分片
+    //- @data[in]: those to be encoded
+    //- @table[in]: code-table for compression
+    fn encode(data: &[Byte], table: &EncodeTable) -> Encoded {
+        //计算编码结果所需空间，超过usize最大值会**panic**
+        let mut len = data.iter().map(|i| table[*i as usize].len()).sum();
+        let pad_len = len % ByteSiz;
+        len = if 0 == pad_len { len / 8 } else { 1 + len / 8 };
+        let mut res = Encoded {
+            data: Vec::with_capacity(len),
+            pad_len: pad_len,
+        };
 
-    //
-    //
-    fn decode(data: &[Byte]) -> Vec<Byte> {}
+        //编码
+        let mut byte_idx = 0usize;
+        let mut bit_idx = 0usize;
+        for i in 0..data.len() {
+            for j in table[data[i] as usize] {
+                res.data[byte_idx] = set_bit(res.data[byte_idx], bit_idx);
+                bit_idx += 1;
+                bit_idx %= ByteSiz;
+                if 0 == bit_idx {
+                    byte_idx += 1;
+                }
+            }
+        }
+
+        res
+    }
+
+    //> 首先解码全体数据，之后再将末尾pad_len的数据弹出
+    //- #: 若在末尾pad_len位数据之前出现解码错误，返回Err(())，否则返回Ok(Vec<Byte>)
+    //- @encoded[in]: encoded data and meta
+    //- @tree[in]: the huffman tree which data has been encoded by
+    fn decode(encoded: &Encoded, tree: &HuffmanTree) -> Result<Vec<Byte>, ()> {
+        let mut res = vec![];
+        let mut t = tree;
+        let mut byte_idx = 0usize;
+        let mut bit_idx = 0usize;
+
+        loop {
+            //非叶点节上不会有数据
+            if let Some(v) = t.data {
+                res.push(v);
+                t = tree;
+            } else {
+                walk_on_tree!(encoded, t, byte_idx, bit_idx);
+            }
+        }
+
+        if 0 < encoded.pad_len {
+            byte_idx = encoded.data.len() - 1;
+            bit_idx = ByteSiz - encoded.pad_len;
+            loop {
+                //非叶点节上不会有数据
+                if let Some(_) = t.data {
+                    res.pop();
+                    t = tree;
+                } else {
+                    walk_on_tree!(encoded, t, byte_idx, bit_idx);
+                }
+            }
+        }
+
+        Ok(res)
+    }
 }
 
+//> n取值范围：[0, 7]
 //- #: 若第n位bit为1，返回true，否则返回false
 #[inline]
-fn check_bit(data: Byte, n: ByteLen) -> bool {
-    data & BitSet[n]
+fn check_bit(data: Byte, n: usize) -> bool {
+    if 0 < data & BitSet[n] {
+        true
+    } else {
+        false
+    }
 }
 
-//> 返回一个Byte比转入指针更廉价
+//> It is cheaper to return a Byte than use a pointer
 #[inline]
-fn set_bit(data: Byte, n: ByteLen) -> Byte {
+fn set_bit(data: Byte, n: usize) -> Byte {
     data | BitSet[n]
 }
 
-//> generate encoded result
+/*
+//> 负责除最后一个Byte之外的数据编码；
+//> 最后一个字节因可能含有pad位，将在上层调用处特殊处理
 //- #: the final encoded data
 //- @bits[in]: assert!(0 == bits.len() % 8)
 //- @res[out]: encoded result will be written here
@@ -81,55 +184,13 @@ fn encode_gen_res(bits: &[Byte], res: Vec<Byte>) -> Vec<Byte> {
         base += 8;
     }
 }
-
-struct Encoded<'a> {
-    //the huffman tree which data has been encoded by
-    tree: &'a HuffmanTree,
-    //encoded data received from some sender[s]
-    data: Vec<Byte>,
-    //the number of bit[s] at the last byte for aligning to ByteSiz
-    pad_len: ByteLen,
-}
-
-//> generate decoded result
-//- #: success will be Ok(Vec<Byte>), otherwise Err(())
-//- @encoded[in]: encoded data and meta
-//- @res[out]: decoded result will be written here
-fn decode_gen_res(encoded: Encoded, res: Vec<Byte>) -> Result<Vec<Byte>, ()> {
-    let mut t = tree;
-    let mut offset = 0;
-
-    for i in 0..data.len() {
-        for j in offset..8 {
-            if check_bit(base[i], j) {
-                t = t.right;
-                if None == t.data {
-                    continue;
-                } else {
-
-                }
-            } else {
-                t = t.left;
-                if None == t.data {
-                    continue;
-                } else {
-
-                }
-            }
-        }
-
-        res.push(t.data.unwrap());
-        t = tree;
-    }
-
-    Ok(())
-}
+*/
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use rayon::prelude::*;
-    use std::collections::HashMap;
+    //use super::*;
+    //use rayon::prelude::*;
+    //use std::collections::HashMap;
 
     #[test]
     fn huffman() {}
